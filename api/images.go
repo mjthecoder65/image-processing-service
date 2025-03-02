5647f40bb4637e9267c740183e992c401ea06d11
Michael <mjthecoder53161@gmail.com>
2025-03-02 22:36:38 +0900

    feat: add endpoint uploading images

---
+++ api/images.go
@@ -0,0 +1,23 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+func (server *Server) uploadImage(ctx *gin.Context) {
+
+}
+
+func (server *Server) getImage(ctx *gin.Context) {
+
+}
+
+func (server *Server) listImages(ctx *gin.Context) {
+
+}
+
+func (server *Server) transformImage(ctx *gin.Context) {
+
+}
+
+func (server *Server) generateImage(ctx *gin.Context) {
+
+}
