d5fe82ba9efa70a6bcf8736951f83e7a967517e4
Michael <mjthecoder53161@gmail.com>
2025-03-02 17:06:34 +0900

    feat: add queries for uploaded images

---
+++ db/query/image.sql
@@ -0,0 +1,14 @@
+-- name: GetUserImages :many
+SELECT
+    *
+FROM 
+    images
+WHERE 
+    user_id = $1
+OFFSET $2 LIMIT $3;
+
+-- name: GetImage :one
+SELECT * FROM images WHERE id = $1;
+
+-- name: CreateImage :one
+INSERT INTO images (name, user_id, url) VALUES ($1, $2, $3) RETURNING *;
--- db/query/user.sql
+++ db/query/user.sql
@@ -2,9 +2,7 @@
 SELECT * FROM users WHERE id = $1;
 
 -- name: GetUserByEmail :one
-SELECT 
-    * 
-FROM 
-    users
-WHERE 
-    email = $1
+SELECT * FROM users WHERE email = $1;
+
+-- name: CreateUser :one
+INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING *;
