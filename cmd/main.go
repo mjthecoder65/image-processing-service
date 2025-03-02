265852b2385e26afd9c5d0237ce9ca5a363a7a91
Michael <mjthecoder53161@gmail.com>
2025-03-02 22:36:05 +0900

    refactor: remove unused dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/mjthecoder65/image-processing-service/api"
 	"github.com/mjthecoder65/image-processing-service/config"
 )
 
 func main() {
+	ctx := context.Background()
 
 	config, err := config.LoadConfig()
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return
 	}
 
-	server, err := api.NewServer(config)
+	conn, err := pgx.Connect(context.Background(), config.DatabaseURL)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close(ctx)
+
+	server, err := api.NewServer(config, conn)
 
 	if err != nil {
 		log.Fatal(err)
@@ -27,5 +42,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
