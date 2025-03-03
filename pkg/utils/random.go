f67843eed97fbf5e051f3d260fa349df83541e2f
Michael <mjthecoder53161@gmail.com>
2025-03-03 14:40:27 +0900

    feat: add function for generating random string

--- pkg/utils/random.go
+++ pkg/utils/random.go
@@ -1,1 +1,16 @@
 package utils
+
+import "math/rand"
+
+const CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func RandomString(length int) string {
+
+	b := make([]byte, length)
+
+	for i := range b {
+		b[i] = CHARSET[rand.Intn(len(CHARSET))]
+	}
+
+	return string(b)
+}
