6d9451f9832c7e7cf83a0ccc60ac6324585f0013
Michael <mjthecoder53161@gmail.com>
2025-03-03 21:40:57 +0900

    refactor: move expire duration to configs list

--- config/config.go
+++ config/config.go
@@ -3,18 +3,21 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	ServerPort    string
-	JWTSecret     string
-	StorageRegion string
-	StorageKey    string
-	StorageSecret string
-	DatabaseURL   string
-	BucketName    string
+	ServerPort           string
+	JWTSecret            string
+	StorageRegion        string
+	StorageKey           string
+	StorageSecret        string
+	DatabaseURL          string
+	BucketName           string
+	JWTExpirationMinutes time.Duration
 }
 
 func LoadConfig() (*Config, error) {
@@ -24,14 +27,21 @@ func LoadConfig() (*Config, error) {
 		return nil, err
 	}
 
+	expirationMinutes, err := strconv.Atoi(getEnvWithDefaultValue("JWT_EXPIRATION_MINUTES", "60"))
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse JWT_EXPIRATION_MINUTES: %w", err)
+	}
+
 	return &Config{
-		ServerPort:    getEnvWithDefaultValue("SERVER_PORT", ":8080"),
-		JWTSecret:     getEnvWithDefaultValue("JWT_SECRET", "Bk1Rqg1Vl2oktA1pTpIbzbZAeWIbus"),
-		StorageRegion: getEnvWithDefaultValue("STORAGE_REGION", "ap-northeast-2"),
-		StorageKey:    getEnvWithDefaultValue("STORAGE_KEY", ""),
-		StorageSecret: getEnvWithDefaultValue("STORAGE_SECRET", ""),
-		DatabaseURL:   getEnvWithDefaultValue("DATABASE_URL", "postgres://localhost:5432"),
-		BucketName:    getEnvRequired("BUCKET_NAME"),
+		ServerPort:           getEnvWithDefaultValue("SERVER_PORT", ":8080"),
+		JWTSecret:            getEnvWithDefaultValue("JWT_SECRET", "Bk1Rqg1Vl2oktA1pTpIbzbZAeWIbus"),
+		JWTExpirationMinutes: time.Duration(expirationMinutes),
+		StorageRegion:        getEnvWithDefaultValue("STORAGE_REGION", "ap-northeast-2"),
+		StorageKey:           getEnvWithDefaultValue("STORAGE_KEY", ""),
+		StorageSecret:        getEnvWithDefaultValue("STORAGE_SECRET", ""),
+		DatabaseURL:          getEnvWithDefaultValue("DATABASE_URL", "postgres://localhost:5432"),
+		BucketName:           getEnvRequired("BUCKET_NAME"),
 	}, nil
 }
 
